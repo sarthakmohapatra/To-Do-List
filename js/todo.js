43dc4b93a7a0bde8e9c6a302f6086719d1aeb48f
Sarthak Mohapatra <m.sarthak1614@gmail.com>
2017-11-03 19:13:23 +0530

    indentifying the code

--- js/todo.js
+++ js/todo.js
@@ -1,29 +1,29 @@
 //checkoff specific todos by clicking
-$("ul").on("click", "li", function(){
-  //check if color is grey, then toggle
-  $(this).toggleClass("completed");
+$("ul").on("click", "li", function() {
+	//check if color is grey, then toggle
+	$(this).toggleClass("completed");
 });
 
 //click on X to delete ToDo
-$("ul").on("click", "span", function(event){
-  $(this).parent().fadeOut(500,function(){
-    $(this).remove();
-  });
-  event.stopPropagation();
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
 
 //adding new todos with ENTER in input
-$("input[type = 'text']").keypress(function(event){
-  if(event.which === 13){
-    //grabbing new todo text from input
-    var todoText = $(this).val();
-    $(this).val("");
-    //create a new li and add to ul
-    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
-  }
+$("input[type = 'text']").keypress(function(event) {
+	if (event.which === 13) {
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+	}
 });
 
 //Toggle fading of Input text
-$(".fa-plus").click(function(){
-  $("input[type = 'text']").fadeToggle();
+$(".fa-plus").click(function() {
+	$("input[type = 'text']").fadeToggle();
 });
